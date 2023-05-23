10e70b106be35564cf64264f27ad843caf75cc45
saumaIqor <josedavids123@live.com>
2023-05-23 17:50:30 -0500

    Primer commit

---
+++ src/main/java/com/mycompany/primerprotecto/PrimerProtecto.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.primerprotecto;
+
+/**
+ *
+ * @author Admin
+ */
+public class PrimerProtecto {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
